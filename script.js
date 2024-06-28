ec16ecefffe390278d26119e744909918859bad2
Murayama <fukick@gmail.com>
2024-06-28 11:12:51 +0900

    no-corsを有効にした

--- script.js
+++ script.js
@@ -82,7 +82,8 @@ function api(endpointUrl, familyId, numDay) {
         }),
         headers: {
             "Content-type": "application/json; charset=UTF-8"
-        }
+        },
+        mode: "no-cors"
     })
         .then((response) => response.json())
         .then((json) => console.log(json))
